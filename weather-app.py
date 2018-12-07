24e3ccc82d144735e1190eb80b2083b1198ca4d3
aarif khan <aaruf@aarifs-MacBook-Air.local>
2018-12-07 18:58:54 +0000

    Writing Weather App, couldn't do it in mac, should try in windows or linux

---
+++ weather-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Real time weather app, small project for myself.
+Learning how awesome python3 can be!
+'''
+import request
+
+def main():
+    city = input("type the city or country name to get weather: ")
+    get_data_from_website(city)
+
+def get_data_from_website(name):
+    pass
+
+def get_info_about_city():
+    pass
+
+if __name__ == "__main__":
+    main()
