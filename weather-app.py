425d01564b1ce72b36d812ca1aafd8e53336fdd4
metalGod <metalaarif@gmail.com>
2018-12-10 12:27:10 +0000

    updating the content

--- weather-app.py
+++ weather-app.py
@@ -3,14 +3,18 @@
 Real time weather app, small project for myself.
 Learning how awesome python3 can be!
 '''
-import request
+import requests
 
 def main():
     city = input("type the city or country name to get weather: ")
     get_data_from_website(city)
+    get_info_about_city()
 
 def get_data_from_website(name):
-    pass
+    html_data = "https://www.weatherbug.com/weather-forecast/now/{}".format(name)
+    html_response = requests.get(html_data).text
+#    print(html_response)
+    return html_response
 
 def get_info_about_city():
     pass
