29a3f1b1c0dbf596426f452832927d3284d60440
metalGod <metalaarif@gmail.com>
2018-12-13 10:28:02 +0000

    Added loop to check unlimited times :)

--- Weather-report.py
+++ Weather-report.py
@@ -6,17 +6,22 @@ Used api from https://openweathermap.org
 '''
 import requests
 import bs4
-city = input("Enter the City Name: ")
-html = "http://api.openweathermap.org/data/2.5/weather?q={}&mode=html&appid=2de305846c0ca215654c20574174124d".format(city)
-response = requests.get(html)
-data = response.text
+i = 0
+while i == 0:
+    city = input("Enter the City Name [london,uk] e[x]it: ")
+    city = city.lower().strip()
+    if city != "x":
+        html = "http://api.openweathermap.org/data/2.5/weather?q={}&mode=html&appid=2de305846c0ca215654c20574174124d".format(
+            city)
+        response = requests.get(html)
+        data = response.text
 
-soup = bs4.BeautifulSoup(data, "html.parser")
+        soup = bs4.BeautifulSoup(data, "html.parser")
 
-location = soup.div.string
-temp = soup.find(title="Current Temperature").string
+        location = soup.div.string
+        temp = soup.find(title="Current Temperature").string
 
-print("The temperature of {} is {}".format(location, temp))
-
-
-
+        print("The temperature of {} is {}".format(location, temp))
+    elif city == "x":
+        print("Thanks ba-bye")
+        break
